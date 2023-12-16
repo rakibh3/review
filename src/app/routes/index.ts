27a654bc1ed00c52f5465efd5c406e0a87fdfeb7
Md Rakibul Hasan <hello.rakibh@gmail.com>
2023-12-16 23:53:50 +0600

    create router

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import { CourseRoute } from '../modules/course/course.route'
+import { CategoryRouter } from '../modules/category/category.route'
 
 const router = Router()
 
 router.use(CourseRoute)
+router.use(CategoryRouter)
 
 export default router
