63a68028da994b9e1d3956303a4c4e501db93bc4
Md Rakibul Hasan <hello.rakibh@gmail.com>
2023-12-19 22:42:14 +0600

    Comment

--- src/app/modules/review/review.model.ts
+++ src/app/modules/review/review.model.ts
@@ -1,6 +1,7 @@
 import { Schema, model } from 'mongoose'
 import { TReview } from './review.interface'
 
+// Review schema model
 const reviewSchema = new Schema<TReview>({
   courseId: { type: Schema.Types.ObjectId, required: true, ref: 'Course' },
   review: { type: String, required: true },
--- src/app/modules/review/review.route.ts
+++ src/app/modules/review/review.route.ts
@@ -3,6 +3,7 @@ import { ReviewController } from './review.controller'
 
 const router = express.Router()
 
+// Router for reviews
 router.post('/reviews', ReviewController.createReview)
 router.get('/courses/:coursesId/reviews', ReviewController.getAllReviews)
 
